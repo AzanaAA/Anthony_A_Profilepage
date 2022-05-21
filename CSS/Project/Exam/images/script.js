94cd20855a036882b49d415a54b704a57d196967
Anthony <anthonyazana@hotmail.com>
2022-05-21 04:21:04 -0400

    changes

---
+++ CSS/Project/Exam/images/script.js
@@ -0,0 +1,11 @@
+function change(element){
+    element.src = "./assets/succulents-2.jpg"
+}
+
+function changeBack(element){
+    element.src = "./assets/succulents-1.jpg"
+}
+
+function hide(element){
+    element.remove();
+}
