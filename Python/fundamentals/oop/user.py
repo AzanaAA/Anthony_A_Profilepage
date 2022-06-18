d1464dfe921dfeae271340d04af1c50115474af9
Anthony <anthonyazana@hotmail.com>
2022-06-18 00:49:31 -0400

    change

--- Python/fundamentals/oop/user.py
+++ Python/fundamentals/oop/user.py
@@ -4,10 +4,22 @@ class User:
         self.last_name = last_name
         self.email = email
         self.age = age
-        
         self.is_rewards_member = False
         self.gold_card_points = 0
 
         def display_info(self):
-            print(User)
-        
+            print(f"First Name: {self.first_name}")
+            print(f"Last Name: {self.last_name}")
+            print(f"Email: {self.email}")
+            print(f"Age: {self.age}")
+            print(f"Is rewards member? {self.is_rewards_memeber}")
+            print(f"Gold Card Points: {self.gold_card_points}") 
+
+        def enroll(self):
+            self.is_rewards_member = True
+            self.gold_card_points = 200
+        print(self.is_rewards_member)
+        print(self.gold_card_points)
+
+        def spend_points(self,amount):
+            self.gold_card_points= self.gold_card_points - amount
