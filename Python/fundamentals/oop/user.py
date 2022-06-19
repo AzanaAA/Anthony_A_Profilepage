59591b10613f8cbd6c60a5b5b039606ee662f66e
Anthony <anthonyazana@hotmail.com>
2022-06-19 16:07:29 -0400

    changes

--- Python/fundamentals/oop/user.py
+++ Python/fundamentals/oop/user.py
@@ -25,10 +25,15 @@ class User:
 
     def spend_points(self,amount):
         self.gold_card_points = self.gold_card_points - amount
+        if self.gold_card_points < amount:
+            print("Overspending!")
+        return self
 
 user1 = User('Anthony','Azana','AnthonyAzana@codingdojo.com',24)
 
 user2 = User('John','Doe','JohnDoe@codingdojo.com',40)
+
+user3= User('Josh','Glasses','JoshGlasses@codingdojo.com',25)
 
 user1.display_info()
 
@@ -40,3 +45,5 @@ user1.enroll()
 user2.enroll()
 user2.spend_points(80)
 user1,user2.display_info()
+
+user3.spend_points(40)
