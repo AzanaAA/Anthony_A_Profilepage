952c208d1c35990f0d199a85db428be95dee1e82
Anthony <anthonyazana@hotmail.com>
2022-07-02 14:20:23 -0400

    changes

--- Python/flask/playground/server.py
+++ Python/flask/playground/server.py
@@ -1,8 +1,18 @@
-from flask import Flask  # Import Flask to allow us to create our app
-app = Flask(__name__)    # Create a new instance of the Flask class called "app"
-@app.route('/')          # The "@" decorator associates this route with the function immediately following
-def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
-if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-    app.run(debug=True)    # Run the app in debug mode.
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/play')
+def one():
+    return render_template("index.html",num=3,color="blue")
+
+@app.route('/play/<int:num>')
+def two(num):
+    return render_template("index.html", num=num, color="blue")
+
+@app.route('/play/<int:num>/<string:color>')
+def three(num, color):
+    return render_template('index.html', num=num, color=color) 
+
+if __name__=="__main__":
+    app.run(debug=True)
