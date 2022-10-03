5daafe051b203772ced93f499d267c582faa4a20
Anthony <anthonyazana@hotmail.com>
2022-10-03 00:11:41 -0400

    changes

---
+++ MERN/React/prop_it_up/src/App.js
@@ -0,0 +1,43 @@
+import PersonCard from './components/PersonCard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonCard
+      lastName={"Doe"}
+      firstName={"Jane"}
+      age={45}
+      hairColor={"Black"}/>
+
+<PersonCard
+      lastName={"Smith"}
+      firstName={"John"}
+      age={88}
+      hairColor={"Brown"}/>
+
+<PersonCard
+      lastName={"Fillmore"}
+      firstName={"Millard"}
+      age={50}
+      hairColor={"Brown"}/>
+
+<PersonCard
+      lastName={"Smith"}
+      firstName={"Maria"}
+      age={62}
+      hairColor={"Brown"}/>
+
+        <PersonCard
+      lastName={"Azana"}
+      firstName={"Anthony"}
+      age={24}
+      hairColor={"Black"}/>
+
+
+      
+    </div>
+  );
+}
+
+export default App;
