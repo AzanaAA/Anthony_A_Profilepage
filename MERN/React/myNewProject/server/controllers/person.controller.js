4814b9eb19491f655a089d6dda4fd56fd10d4447
Anthony <anthonyazana@hotmail.com>
2022-11-09 21:05:45 -0500

    changes

--- MERN/React/myNewProject/server/controllers/person.controller.js
+++ MERN/React/myNewProject/server/controllers/person.controller.js
@@ -1,8 +1,4 @@
-module.exports.index = (request, response) => {  //We are exporting a key:val pair of index : function
-    response.json({     // This is where we're setting the API's response to the requesting client
-       message: "Hello World"
-    });
-}
+
 const Person = require('../models/person.model');    /* this is new */
 module.exports.index = (request, response) => {
     response.json({
---
+++ MERN/React/productmanager/client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CreateProduct from "./components/CreateProduct";
+
+function App() {
+  return (
+    <div className="App">
+      <CreateProduct/>
+    </div>
+  );
+}
+
+export default App;
